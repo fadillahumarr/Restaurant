2f940e3fbd725de34de00c06cc09bb22c8eace85
Nurfadillah Umar <itsennuu@gmail.com>
2024-03-22 10:27:59 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-activate");
+  navMenu.classList.toggle("hidden");
+});
